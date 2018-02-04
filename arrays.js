0a0c218a5ea4cbdf434eb8e97f35d5f76b43aa83
George  Pianka <gpianka1@gmail.com>
2018-02-04 10:47:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 function removeElementFromBeginningOfArray(array) {
   array.slice(1);
-  return array
+  return array;
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
   
