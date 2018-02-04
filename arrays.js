ae899f2580d638c16c6375e6b5e6d2738646986d
George  Pianka <gpianka1@gmail.com>
2018-02-04 09:54:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,6 @@ function addElementToBeginningOfArray(array, element) {
   var newArray = [element, ...array];
   return newArray;
 }
-function destructivelyAddElementToBeginningOfArray(array,element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
 }
