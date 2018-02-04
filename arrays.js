1c1f585e8cb8508b6e716867aa5667e0b644fa15
George  Pianka <gpianka1@gmail.com>
2018-02-04 10:03:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
 function addElementToBeginningOfArray(array, element) {
-  return [element]
+  return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
